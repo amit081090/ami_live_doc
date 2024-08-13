440ad269d5735886ee4f77b94ce09abc364ce987
Amit Bahuguna <bahugunaamit8@gmail.com>
2024-08-13 11:57:00 +0530

    first commit

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,58 @@
+import CollaborativeRoom from '@/components/CollaborativeRoom'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import { getDocument } from '@/lib/actions/room.actions'
+import React from 'react'
+import { getClerkUsers } from '@/lib/actions/user.actions'
+
+
+const Document = async({params:{id}}:SearchParamProps) => {
+
+  const clerkUser=await currentUser()
+  if(!clerkUser) redirect('/sign-in');
+
+  const room =await getDocument({
+    roomId:id,
+    userId:clerkUser.emailAddresses[0].emailAddress,
+
+  })
+
+  if(!room) redirect('/')
+   
+    const userIds = Object.keys(room.usersAccesses)
+    const users = await getClerkUsers({userIds})
+
+    const usersData=users.map((user:User)=>({
+
+      ...user,
+      userType:room.usersAccesses[user.email]?.includes('room:write')
+
+      ? 'editor'
+      :'viewer'
+
+    }))
+
+    const currentUserType= room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes
+    ('room:write')?'editor': 'viewer';
+     
+
+
+
+  return (
+    <div>
+      <main className='flex w-full flex-col items-center'>
+       <CollaborativeRoom
+       
+       roomId={id}
+       roomMetadata={room.metadata}
+       users={usersData}
+       currentUserType={currentUserType}
+       />
+
+      </main>
+     
+      </div>
+  )
+}
+
+export default Document
