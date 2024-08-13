f1ef8d351221202296b235d13551dce5bc2155b7
Amit Bahuguna <bahugunaamit8@gmail.com>
2024-08-13 12:15:09 +0530

    fixed typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils"
 import { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import {dark} from "@clerk/themes"
-import Provider from "./provider";
+import Provider from "./Provider";
 
 
 const fontSans = FontSans({
